2e4d2aef7288a66e34247b0a5add5646d9756e0c
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-05-16 10:45:07 +0100

    update packages and remove logic from index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-import { ZOOPLA_API_BASE_PATH, ZOOPLA_API_HOSTNAME, PROPERTY_LISTING_ENDPOINT } from './constants';
-import httpClient from './HttpClient';
-
-class Zoopla {
-    constructor({ apiKey }) {
-        this.apiKey = apiKey;
-    }
-
-    getPropertyListings(parameters) {
-        return this.request({ endpoint: PROPERTY_LISTING_ENDPOINT, parameters });
-    }
-
-    getPropertyListingsById(...ids) {
-        const parameters = { listing_id: ids };
-        return this.request({ endpoint: PROPERTY_LISTING_ENDPOINT, parameters });
-    }
-
-    request({ endpoint, parameters }) {
-        const url = `${ZOOPLA_API_HOSTNAME}${ZOOPLA_API_BASE_PATH}${endpoint}`;
-        const query = {
-            ...parameters,
-            api_key: this.apiKey,
-        };
-
-        return httpClient.get({ url, query });
-    }
-}
+import Zoopla from './Zoopla';
 
 module.exports = Zoopla;
